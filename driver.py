b53b484ccbb904ecf8eeede3f80173bea5d1e1f3
jyang077 <112518069+jyang077@users.noreply.github.com>
2023-02-27 22:22:59 -0500

    comments added

--- driver.py
+++ driver.py
@@ -12,8 +12,6 @@ args = ap.parse_args()
 input_path = getattr(args, 'input')
 
 # checks if input is a valid video/image file, if not, prints an error message.
-# potential edge case if someone tries to upload a file called .jpg.wav or any sort of incompatible file that also happens
-# to have a compatible file name somewhere in it
 if input_path.endswith(".jpg") | input_path.endswith(".png"):
     process_image(input_path)
 elif input_path.endswith(".mp4"):
